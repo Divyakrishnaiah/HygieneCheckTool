2cb7b2fbdf417d0e884fb19d6aada4b2c7291676
ABHISEK <abhisek@abhisek-macbook.local>
2020-02-15 17:51:08 +0530

    First commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/commonkeywords")
+def commonkeywords():
+    return render_template("commonkeywords.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8970)
